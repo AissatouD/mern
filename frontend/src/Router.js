49d11aabaf48c687c92955cecec6d4cf9e2de4f4
Diop <aissatoudiop96@gmail.com>
2020-04-01 00:15:40 +0200

    sign in

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Inventory from './components/Inventory/Inventory';
 import NotFound from './components/NotFound/NotFound';
+import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 
 const Router = () => (
@@ -11,6 +12,7 @@ const Router = () => (
       <Route exact path='/' component={Home} />
       <Route exact path='/Inventory' component={Inventory} />
       <Route exact path='/SignUp' component={SignUp} />
+      <Route exact path='/SignIn' component={SignIn} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
